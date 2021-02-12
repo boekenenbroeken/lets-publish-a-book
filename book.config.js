65bb1b5a6c301db161683014844590128a96317e
almost_bergman <almost.bergman@gmail.com>
2021-02-12 18:39:54 +0300

    add test case

---
+++ book.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  entry: 'test/test.docx',
+  dist: 'generated',
+  fileName: 'book',
+  chaptersSelector: 'h1, h2, h3',
+  cover: 'test/cover.png',
+  lang: 'ru',
+  title: 'Test',
+  author: 'Test Author',
+  tocTitle: 'Table of Contents',
+  publisher: 'Buchweizen'
+}
